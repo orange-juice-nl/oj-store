bb5288638eadf9572e92f9c8e80bb70c420630e7
Marius Linders <marius.linders@orange-juice.nl>
2020-03-13 12:59:14 +0100

    code cleanup, optimizations and bugfixes, readme updated

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Store
+A complete state / store solution
+Get the benefits of immutable data with the ease of mutable data.
+History (undo / redo, persistent) support with deltas (only the diffs are stored).
+Support for async (Promise) reducers.
+Subscribe to store events.
 
 ## Usage
 
@@ -11,19 +16,48 @@ import { Store } from "oj-store";
 ```typescript
 const initialState = {
   name: "Orange Juice",
-  deep: { nested: [ { data: "with immer" } ] }
+  deep: { nested: [{ data: "is easy" }] }
 }
-const store = new Store(initialState, {
-  history: {
-    set: state => localForage.setItem("history", state),
-    get: localForage.getItem("history")
-  },
-  reducers: {
-    setName: (name:string) => draft => { draft.name = name },
-    pushDeepNested: (data: string) => draft => { draft.deep.nested.push({ data }) },
-    reset: () => () => initialState
-  }
-})
+const store = new Store(initialState)
+```
+
+#### Reducers
+```typescript
+const reducers = {
+  setName: (name: string) => draft => { draft.name = name },
+  pushDeepNested: (data: { data: string }) => draft => { draft.deep.nested.push(data) },
+  setNameFromApi: () => 
+    fetch("url/user")
+      .then(x => x.json())
+      .then(x => draft => { draft.name = x.name})
+}
+const store = new Store(initialState, { reducers })
+```
+
+#### History
+The history option can be true, false or an object with a getter and setter.
+You can use the getter and setter for persistency, for example with the localForage package.
+
+```typescript
+const history = {
+  set: deltas => localForage.setItem("history", deltas),
+  get: () => localForage.getItem("history")
+}
+const store = new Store(initialState, { history, reducers })
+```
+
+#### Middleware
+```typescript
+const middleware = [
+    state => next => reducer => {
+      const s = state()
+      const nextState = reducer(s)
+      console.log("logger", s, nextState)
+      next(() => nextState)
+    }
+]
+
+const store = new Store(initialState, { history, reducers, middleware })
 ```
 
 ### Get state
@@ -32,87 +66,61 @@ const current = store.state()
 /*
 {
   name: "Orange Juice",
-  deep: { nested: [ { data: "with immer" } ] }
+  deep: { nested: [ { data: "is easy" } ] }
 }
 */
 ```
 
 ### Dispatch reducer
 ```typescript
-store.dispatch.setName("new name")
+store.dispatch.setName("Awesome")
+store.dispatch.pushDeepNested({ data: "super easy" })
 /*
 {
-  name: "new name",
-  deep: { nested: [ { data: "with immer" } ] }
+  name: "Awesome",
+  deep: { nested: [{ data: "is easy" }, { data: "super easy" }] }
 }
 */
 ```
 
 ### Reduce
 ```typescript
-store.reduce(draft => {
-  draft.deep.nested[0].data = "awesome"
-})
+store.reduce(d => { d.name = "Awesome" })
+store.reduce(d => { d.deep.nested.push({ data: "super easy" }) })
+store.reduce(d => ({ name: "Reset", deep: { nested: [] })) // set full state
+
+console.log(store.state())
 /*
 {
-  name: "new name",
-  deep: { nested: [ { data: "awesome" } ] }
-}
-*/
-```
-```typescript
-store.reduce(async draft => {
-  draft.name = await getName() // "fetched name"
-})
-/*
-{
-  name: "fetched name",
-  deep: { nested: [ { data: "awesome" } ] }
+  name: "Awesome",
+  deep: { nested: [{ data: "is easy" }, { data: "super easy" }] }
 }
 */
 ```
 
 ### Undo / Redo
+check if undo/redo is possible
+
 ```typescript
   button.classList.toggle("disabled", !store.canUndo())
-```
-```typescript
   button.classList.toggle("disabled", !store.canRedo())
 ```
+
+undo / redo
+
 ```typescript
-  store.undo()
-/*
-{
-  name: "new name",
-  deep: { nested: [ { data: "awesome" } ] }
-}
-*/
-```
-```typescript
-  store.redo()
-/*
-{
-  name: "fetched name",
-  deep: { nested: [ { data: "awesome" } ] }
-}
-*/
-```
-```typescript
-  store.undo(2) // undo the last 5 changes
-/*
-{
-  name: "new name",
-  deep: { nested: [ { data: "with immer" } ] }
-}
-*/
+  store.undo() // undo last change
+
+  store.redo() // redo last undo
+
+  store.undo(4) // undo last 4 changes
 ```
 
 ## Events
 Uses the oj-eventaggregator package for handling Events
 
 ### Change
-The change event is emitted on every state change caused by store.reduce(...), store.dispatch[reducer](...), store.undo(...) or store.redo(...) actions.
-
+The change event is emitted after every state change caused by store.reduce(...), store.dispatch[reducer](...), store.undo(...) or store.redo(...) actions.
 ```typescript
 store.on("change", getState => {
   console.log(getState())
@@ -137,6 +145,15 @@ store.on("redo", getState => {
 })
 ```
 
+### Load
+The load event is emitted when the store is initialized and the history is fully restored (if any).
+
+```typescript
+store.on("change", getState => {
+  console.log(getState())
+})
+```
+
 ## Types
 
 ### Store
@@ -151,16 +168,35 @@ R is the store reducers object
 (...args: any[]) => (draft: Draft<T>) => void | Draft<T>
 ```
 
-### IReducers<T>
+### IReducerAsync<T>
 ```typescript
-{ [k: string]: IReducer<T> }
+(...args: any[]) => Promise<(draft: Draft<T>) => void | Draft<T>>
+```
+
+### IReducers<R, T>
+```typescript
+{ [k: string]: IReducer<T> | IReducerAsync<T> }
+```
+
+### IMiddleware<T>
+```typescript
+(state: () => T) => (next: (state: T) => void) => (reducer: any) => any
+```
+
+### IStoreOptionsHistory<T>
+```typescript
+{
+  set: (deltas: Delta[]) => any,
+  get: () => Promise<Delta[]> | Delta[] | void,
+  ready?: (getState: () => T) => any
+}
 ```
 
 ### IStoreOptions<R, T>
 ```typescript
 {
-  history?: boolean | { set: (deltas: Delta[]) => any, get: () => Promise<Delta[]>, ready?: () => any }
-  reducers?: R & IReducers<T>,
-  middleware?: Array<(store: any) => (next: any) => (reducer: any) => any>
+  history?: boolean | IStoreOptionsHistory<T>
+  reducers?: IReducers<R, T>,
+  middleware?: IMiddleware<T>[]
 }
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,34 @@
-import { Store as Repatch, Unsubscribe, Middleware, Reducer } from "repatch"
+import { Store as Repatch, Middleware, Reducer } from "repatch"
 import { DiffPatch, Delta } from "oj-diff-patch"
 import { produce, Draft, setAutoFreeze } from "immer"
 import { EventAggregator } from "oj-eventaggregator"
 
 setAutoFreeze(false)
 
+export { Delta, Draft }
+
 export type IReducer<T> = (...args: any[]) => (draft: Draft<T>) => void | Draft<T>
+export type IReducerAsync<T> = (...args: any[]) => Promise<(draft: Draft<T>) => void | Draft<T>>
+export type IReducers<R, T> = R & { [k: string]: IReducer<T> | IReducerAsync<T> }
 
-export type IReducers<T> = { [k: string]: IReducer<T> }
-
-export interface IStoreOptions<R, T> {
-  history?: boolean | { set: (deltas: Delta[]) => any, get: () => Promise<Delta[]>, ready?: () => any }
-  reducers?: R & IReducers<T>,
-  middleware?: Array<(store: any) => (next: any) => (reducer: any) => any>
+export interface IStoreOptionsHistory<T> {
+  set: (deltas: Delta[]) => any,
+  get: () => Promise<Delta[]> | Delta[] | void,
+  ready?: (state: () => T) => any
 }
 
-export class Store<T extends Object, R> extends EventAggregator<"change" | "undo" | "redo"> {
-  public readonly dispatch: IStoreOptions<R, T>["reducers"] = {} as IStoreOptions<R, T>["reducers"];
-  private readonly diffPatch: DiffPatch<T>
-  private readonly repatch: Repatch<T>
+export type IMiddleware<T> = (state: () => T) => (next: (state: T) => void) => (reducer: any) => any
+
+export interface IStoreOptions<R, T> {
+  history?: boolean | IStoreOptionsHistory<T>
+  reducers?: IReducers<R, T>,
+  middleware?: IMiddleware<T>[]
+}
+
+export class Store<T extends Object, R> extends EventAggregator<"change" | "undo" | "redo" | "load"> {
+  public dispatch: IStoreOptions<R, T>["reducers"] = {} as IStoreOptions<R, T>["reducers"];
+  private diffPatch: DiffPatch<T>
+  private repatch: Repatch<T>
   private options: IStoreOptions<R, T> = {};
 
   constructor(initial: T = {} as T, options: IStoreOptions<R, T> = {}) {
@@ -26,67 +36,20 @@ export class Store<T extends Object, R> extends EventAggregator<"change" | "undo
     this.options = options
     this.repatch = new Repatch(initial)
 
-    if (this.options.history) {
-      this.diffPatch = new DiffPatch()
-      this.diffPatch.add(initial)
-      const mw = store => next => reducer => {
-        const state = store.getState()
-        const nextState = reducer(state)
-        if (nextState["__ignoreHistory"] !== true)
-          this.diffPatch.add(nextState)
-        delete nextState["__ignoreHistory"]
-        return next(() => nextState)
-      }
-      this.repatch.addMiddleware(mw)
+    this.initHistory(initial)
 
-      if (typeof this.options.history === "object") {
-        const h = this.options.history as any
-        if (typeof h.get === "function")
-          h.get().then(d => {
-            const state = this.diffPatch.load(d)
-            this.repatch.dispatch(() => {
-              state["__ignoreHistory"] = true
-              return state
-            })
-            if (typeof h.ready === "function")
-              h.ready()
-          })
-        if (typeof h.set === "function")
-          this.diffPatch.listen = deltas => h.set(deltas)
-      }
-    }
-
-    const mw = store => next => reducer => {
-      const state = store.getState()
-      const nextState = reducer(state)
-      if (typeof nextState === "function") {
-        const promise = nextState(this)
-        if (!(promise instanceof Promise))
-          throw new Error(`Async function should return a Promise`)
-        promise.then(state => this.repatch.dispatch(() => state))
-        return next(() => state)
-      }
-      return next(() => nextState)
-    }
-    this.repatch.addMiddleware(mw)
+    this.initMiddleware()
 
     if (options.middleware)
-      this.repatch.addMiddleware(...options.middleware)
+      this.repatch.addMiddleware(...options.middleware.map(x => store => x(store.getState) as any))
 
-    if (options.reducers)
-      Object.keys(options.reducers)
-        .forEach(k =>
-          (this.dispatch as any)[k] = (...args: any[]) => {
-            this.repatch.dispatch(state =>
-              produce(state, draft => options.reducers[k](...args)(draft) as any))
-            this.emit("change", () => this.state())
-            return this
-          })
+    this.initReducers(options.reducers)
   }
 
   reduce(mutate: (draft: Draft<T>) => void | Draft<T>) {
     this.repatch.dispatch(state =>
-      produce(state, draft => mutate(draft) as any))
+      produce(state, draft =>
+        mutate(draft) as any))
     this.emit("change", () => this.state())
     return this
   }
@@ -136,5 +99,96 @@ export class Store<T extends Object, R> extends EventAggregator<"change" | "undo
 
   state() {
     return this.repatch.getState()
+  }
+
+  private async initHistory(initial: T) {
+    if (!this.options.history) {
+      setTimeout(() => this.emit("load", () => this.state()), 0)
+      return
+    }
+
+    this.diffPatch = new DiffPatch()
+    this.diffPatch.add(initial)
+
+    const mw = store => next => reducer => {
+      const state = store.getState()
+      const nextState = reducer(state)
+      if (nextState["__ignoreHistory"] !== true)
+        this.diffPatch.add(nextState)
+      delete nextState["__ignoreHistory"]
+      return next(() => nextState)
+    }
+    this.repatch.addMiddleware(mw)
+
+    if (typeof this.options.history === "object") {
+      await this.initHistoryGetSet()
+      this.emit("load", () => this.state())
+    }
+    else
+      setTimeout(() => this.emit("load", () => this.state()), 0)
+  }
+
+  private restoreDeltas(deltas: Delta[]) {
+    const state = this.diffPatch.load(deltas)
+    if (!state)
+      return
+
+    this.repatch.dispatch(() => {
+      state["__ignoreHistory"] = true
+      return state
+    })
+  }
+
+  private async initHistoryGetSet() {
+    const h = this.options.history as IStoreOptionsHistory<T>
+
+    if (typeof h.get === "function") {
+      const res = h.get()
+      if (res) {
+        if (Array.isArray(res))
+          this.restoreDeltas(res)
+        else
+          this.restoreDeltas(await res)
+      }
+    }
+
+    if (typeof h.set === "function") {
+      this.diffPatch.listen = deltas => h.set(deltas)
+    }
+
+    if (typeof h.ready === "function")
+      h.ready(() => this.state())
+  }
+
+  private initMiddleware() {
+    const mw = store => next => reducer => {
+      const state = store.getState()
+      const nextState = reducer(state)
+      if (typeof nextState === "function") {
+        const promise = nextState(this)
+        if (!(promise instanceof Promise))
+          throw new Error(`Async function should return a Promise`)
+        promise.then(state => this.repatch.dispatch(() => state))
+        // return next(() => state)
+      }
+      return next(() => nextState)
+    }
+    this.repatch.addMiddleware(mw)
+  }
+
+  private initReducers(reducers?: IReducers<R, T>) {
+    if (!reducers)
+      return
+
+    Object.keys(reducers)
+      .forEach(k =>
+        (this.dispatch as any)[k] = (...args: any[]) => {
+          const reducer = reducers[k](...args)
+          if (reducer instanceof Promise)
+            reducer.then(r => this.reduce(draft => r(draft)))
+          else
+            this.reduce(draft => reducer(draft))
+          return this
+        })
   }
 }
